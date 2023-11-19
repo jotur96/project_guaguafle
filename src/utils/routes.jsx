affe53c279b70d48823553767afca06cf11ca55c
Julio Olmedo <turlan125@gmail.com>
2023-11-19 19:18:27 -0300

    Add routes and update App component

---
+++ src/utils/routes.jsx
@@ -0,0 +1,27 @@
+import { useRoutes } from "react-router-dom"
+import { Inicio } from "../views/inicio/Inicio"
+import { Nosotros } from "../views/nosotros/Nosotros"
+import { Menu } from "../views/menu/Menu"
+
+
+export const AppRoutes = () => {
+    const routes = useRoutes([
+        {
+            path: '/',
+            element: <Inicio />
+        },
+        {
+            path: '/menu',
+            element: <Menu />
+        },
+        {
+            path: '/nosotros',
+            element: <Nosotros />
+        },
+        {
+            path: '*',
+            element: <Inicio />
+        }
+    ])
+
+}
