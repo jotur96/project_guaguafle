b50fc4b9c5f0d95a5e7cf8e550ace4be07c919c3
Victoria Domaniczky <vicdomapy@gmail.com>
2023-11-19 23:37:18 -0300

    Update nav
    Menú desplegable para móvil
    Estilos

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,76 @@
-import { NavLink } from "react-router-dom"
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const NavItem = ({ to, children, onClick }) => {
+  const activeStyle = 'underline underline-offset-4 font-semibold';
+  return (
+    <li className="">
+      <NavLink
+        to={to}
+        onClick={onClick}
+        className={({ isActive }) => isActive ? activeStyle : ""}>
+        {children}
+      </NavLink>
+    </li>
+  );
+};
 
 
-const NavItem = ({ to, children }) => {
-    const activeStyle = 'underline underline-offset-4 font-semibold'
-    return (
-      <li>
-        <NavLink
-          to={to}
-          className={({ isActive }) =>  isActive ? activeStyle : ""}>
-          {children}
-        </NavLink>
-      </li>
-    );
-  };
+const MobileMenuToggle = ({ onToggle }) => {
+  return (
+    <div className="lg:hidden">
+      <button onClick={onToggle} className="text-white focus:outline-none">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+        </svg>
+      </button>
+    </div>
+  );
+};
 
+const MobileMenuContent = ({ isOpen, onClose }) => {
+  return (
+    <div>
+      <div>
+        <button onClick={onClose} className="text-white focus:outline-none">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+          </svg>
+        </button>
+      </div>
+      <div className={`fixed top-0 left-0 w-full h-50 bg-red-500 flex flex-col items-center p-8 ${isOpen ? 'block' : 'hidden'}`}>
+        <NavItem to='/' onClick={onClose}>Inicio</NavItem>
+        <NavItem to='/menu' onClick={onClose}>Menú</NavItem>
+        <NavItem to='/nosotros' onClick={onClose}>Nosotros</NavItem>
+      </div>
+    </div>
+  );
+};
 
 export const Nav = () => {
-    return (
-        <nav className="flex justify-end items-end z-10 w-full top-0 py-5 px-8 text-sm font-dark
-        bg-red-500">
-            <ul className="flex gap-3 items-end">
-                <NavItem to='/'>Inicio</NavItem>
-                <NavItem to='/menu'>Menú</NavItem>
-                <NavItem to='/nosotros'>Nosotros</NavItem>
-            </ul>
-        </nav>
-    )
-}
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <nav className="flex justify-between items-end z-10 w-full top-0 py-5 px-8 text-xl font-dark bg-red-500">
+      
+      <MobileMenuToggle onToggle={toggleMobileMenu} />
+      <MobileMenuContent isOpen={isMobileMenuOpen} onClose={closeMobileMenu} />
+      <div className="lg:flex">
+        <ul className="flex gap-3 items-end">
+          <NavItem to='/' onClick={closeMobileMenu}>Inicio</NavItem>
+          <NavItem to='/menu' onClick={closeMobileMenu}>Menú</NavItem>
+          <NavItem to='/nosotros' onClick={closeMobileMenu}>Nosotros</NavItem>
+        </ul>
+      </div>
+      
+    </nav>
+  );
+};
