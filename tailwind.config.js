92abfc1cf2df98d8c40a840f636492e350b6fae6
Julio Olmedo <turlan125@gmail.com>
2023-11-19 23:25:55 -0300

    Add react-icons package and update Footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        guaflePrimary: '#FF3B00',
+        guafleSecondary: '#FAEFDF',
+      }
+    },
   },
   plugins: [],
 }
