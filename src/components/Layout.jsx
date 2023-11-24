52a55a28a2d5496e1eb01e9d3294c3e17fe5b9d6
Julio Olmedo <turlan125@gmail.com>
2023-11-23 21:06:15 -0300

    refactor menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Children } from "react"
 
 export const Layout = ( {children} ) => {
     return (
-        <div className="flex justify-center bg-guafleSecondary h-full w-full">
+        <div className="flex justify-center bg-guafleSecondary h-full w-full md:mt-16 mt-32">
             {children}
         </div>
     )
