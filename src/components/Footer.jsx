e4d6498cec6e8ef64b0edfaf6909360739490a76
Julio Olmedo <turlan125@gmail.com>
2023-11-24 15:22:20 -0300

    Fix social media links and layout styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,35 +26,25 @@ export const Footer = () => {
                         </div>
                     </div>
                     <div className='flex mx-auto justify-evenly pt-10 pb-5 md:pt-5'>
-                        <a href="https://facebook.com" target="_blank">
-                        <FaFacebook className="text-4xl  transition ease-in-out delay-1 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-blue-600 cursor-pointer" />
                         <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                        <FaFacebook className="text-4xl  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-blue-600 cursor-pointer" />
+                            <FaFacebook className="text-4xl  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-blue-600 cursor-pointer" />
                         </a>
-                        <a href="https://instagram.com" target="_blank">
-                        <FaInstagram className="text-4xl hover:text-white from-orange-400 via-red-500 to-purple-800 transition ease-in-out delay-1 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-gradient-to-br rounded-bordeado-ig cursor-pointer" />
                         <a href="https://instagram.com" target="_blank" rel="noreferrer">
-                        <FaInstagram className="text-4xl hover:text-white from-orange-400 via-red-500 to-purple-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-gradient-to-br rounded-bordeado-ig cursor-pointer" />
+                            <FaInstagram className="text-4xl hover:text-white from-orange-400 via-red-500 to-purple-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-gradient-to-br rounded-bordeado-ig cursor-pointer" />
                         </a>
-                        <a href="https://wa.me/595971167440" target="_blank">
-                        <FaWhatsapp className="text-4xl  transition ease-in-out delay-1 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-green-500 cursor-pointer" />
                         <a href="https://wa.me/595971167440" target="_blank" rel="noreferrer">
-                        <FaWhatsapp className="text-4xl  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-green-500 cursor-pointer" />
+                            <FaWhatsapp className="text-4xl  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-green-500 cursor-pointer" />
                         </a>
-                        <a href="https://tiktok.com" target="_blank">
-                        <FaTiktok className="text-4xl transition ease-in-out delay-1 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-white cursor-pointer" />
                         <a href="https://tiktok.com" target="_blank" rel="noreferrer">
-                        <FaTiktok className="text-4xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-black cursor-pointer" />
+                            <FaTiktok className="text-4xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-black cursor-pointer" />
                         </a>
-                        <a href="https://x.com" target="_blank">
-                        <FaTwitter className="text-4xl transition ease-in-out delay-1 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-blue-500 cursor-pointer" />
                         <a href="https://x.com" target="_blank" rel="noreferrer">
-                        <FaTwitter className="text-4xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-blue-500 cursor-pointer" />
+                            <FaTwitter className="text-4xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:text-blue-500 cursor-pointer" />
                         </a>
 
                     </div>
                 </div>
-                <div className="flex justify-center py-10 mx-auto w-full md:w-2/4"> 
+                <div className="flex justify-center py-10 mx-auto w-full md:w-2/4">
                     <iframe className="rounded-3xl w-96 h-80 md:w-full md:h-80" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3607.3695943561415!2d-57.561572523645566!3d-25.291783677650233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x945da9862b5dfa21%3A0x4fc9830f71ff6260!2sGuaguafle!5e0!3m2!1ses-419!2spy!4v1700443064039!5m2!1ses-419!2spy"></iframe>
                 </div>
 
