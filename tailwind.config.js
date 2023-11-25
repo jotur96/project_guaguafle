2939f5d77ec8132a0a46ec1665e3ab1d96f38e55
Julio Olmedo <turlan125@gmail.com>
2023-11-25 11:48:38 -0300

    Add responsive screen sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-export default withMT( {
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -25,6 +25,22 @@ export default withMT( {
         '600': '600px',
         'screen-50': '50vh',
       },
+    },
+    screens: {
+      'sm': { 'min': '640px', 'max': '767px' },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': { 'min': '768px', 'max': '1023px' },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': { 'min': '1024px', 'max': '1279px' },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': { 'min': '1280px', 'max': '1535px' },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': { 'min': '1536px' },
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
