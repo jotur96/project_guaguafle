aa41f3a2029e894f5e89e03ca59efbc29ad3df4a
Julio Olmedo <turlan125@gmail.com>
2023-11-27 12:45:46 -0300

    Update layout and navigation styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Children } from "react"
 
 export const Layout = ( {children} ) => {
     return (
-        <div className="flex justify-center bg-guafleSecondary h-full w-full pt-40 2xl:pt-28">
+        <div className="flex justify-center bg-guafleSecondary h-full w-full pt-20 md:pt-28  2xl:pt-28">
             {children}
         </div>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,13 +5,15 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import logoBlanco from "../assets/images/logo/blanco-sin-tag.png"
 import logoNegro from "../assets/images/logo/negro-sin-tag.png"
 
-const NavItem = ({ to, children, onClick }) => {
+const NavItem = ({ to, children }) => {
   const activeStyle = 'underline underline-offset-4 font-semibold';
   return (
     <li className="flex items-center text-black hover:text-guafleSecondary lg:text-4xl px-5">
       <NavLink
         to={to}
-        onClick={onClick}
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: 'smooth' })
+        }}
         className={({ isActive }) => isActive ? activeStyle : ""}>
         {children}
       </NavLink>
@@ -52,32 +54,27 @@ export const Nav = (props) => {
   useEffect(() => {
     window.addEventListener("resize", handleWindowResize);
     document.addEventListener("click", handleOutsideClick);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("resize", handleWindowResize);
       document.addEventListener("click", handleOutsideClick);
+      window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [openNav]);
 
   return (
     <Navbar className="mx-auto max-w-screen-x bg-guaflePrimary py-0 fixed top-0 z-20" fullWidth="true" color="guafleOrange">
       <div className="flex items-center justify-evenly text-white">
-        {/* Logo con imagen */}
-        {/* <img className="h-20 md:h-28 lg:h-32 xl:h-40 2xl:h-28 w-40 object-cover object-center py-0 top-0 transition-opacity duration-300 hover:opacity-0"
-          src={logo} alt="Logo" />
-        <img className="h-20 md:h-28 lg:h-32 xl:h-40 2xl:h-28 w-40 object-cover object-center py-0 top-0 transition-opacity duration-300 hover:opacity-100"
-          src={logo} // Imagen oculta
-          alt="Logo"
-        /> */}
-        <div className="relative">
+        <div className="relative h-20 md:h-28 w-36 md:w-44 top-0   ">
           <a href="/">
           <img
-            className="h-20 md:h-28 lg:h-32 xl:h-40 2xl:h-28 w-40 object-cover object-center py-0 transition-opacity duration-300"
+            className="absolute top-0 object-cover object-center h-full w-full transition-opacity duration-300"
             src={logoBlanco} // Imagen visible
             alt="Logo"
           />
           <img
-            className="h-20 md:h-28 lg:h-32 xl:h-40 2xl:h-28 w-40 object-cover object-center py-0 absolute top-0 transition-opacity duration-300 hover:opacity-0"
+            className="absolute top-0 object-cover object-center h-full w-full transition-opacity duration-300 hover:opacity-0"
             src={logoNegro} // Imagen oculta
             alt="Logo"
           />
@@ -88,14 +85,17 @@ export const Nav = (props) => {
         </div>
         <IconButton
           variant="text"
-          className="ml-auto h-6 w-6 text-white hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          className="ml-auto mr-10 text-white hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
           ripple={false}
-          onClick={() => setOpenNav(!openNav)}
+          onClick={() => {   
+            window.scrollTo({ top: 0})
+            setOpenNav(!openNav);
+          }}
         >
           {openNav ? (
-            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+            <XMarkIcon className="h-10 w-10" strokeWidth={2} />
           ) : (
-            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+            <Bars3Icon className="h-10 w-10" strokeWidth={2} />
           )}
         </IconButton>
       </div>
