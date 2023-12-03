5c73c2259d739878f6444642c97a8d7888b95eb7
Julio Olmedo <turlan125@gmail.com>
2023-12-03 14:50:10 -0300

    Add new images and update styles

--- src/views/inicio/Inicio.jsx
+++ src/views/inicio/Inicio.jsx
@@ -3,44 +3,63 @@ import { Layout } from "../../components/Layout"
 import { Link } from "react-router-dom";
 import { CarouselWithContent } from "../../components/Carousel";
 import { EcommerceCard } from "../../components/Card";
+import img1 from "../../assets/images/inicio-images/1.jpg"
+import img2 from "../../assets/images/inicio-images/2.jpg"
+import img3 from "../../assets/images/inicio-images/3.jpg"
+import img4 from "../../assets/images/inicio-images/4.jpg"
+import img5 from "../../assets/images/inicio-images/5.jpg"
+import img6 from "../../assets/images/inicio-images/6.jpg"
 
 
 export const Inicio = () => {
+
+    const imagesMenu = [
+        {
+            id: 1,
+            img: "https://scontent.fasu11-1.fna.fbcdn.net/v/t39.30808-6/326163101_400058138990872_3448990309395903189_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=3635dc&_nc_ohc=grASwb4LK6AAX8TwT_L&_nc_ht=scontent.fasu11-1.fna&oh=00_AfBi3daRkFd17dUgtnV5U8lSy77gCAk9PWVW8ZuWQkHJHg&oe=656CAB84",
+            alt: "Elegí tu masa"
+        }
+    ]
+
     return (
         <Layout>
-            <div className="w-full h-full flex flex-col items-center z-0">
-                {/* <div className="flex flex-col items-center justify-center xl:w-1/2 2xl:w-fit xl:h-screen-50"> */}
-                    <CarouselWithContent />
-                {/* </div> */}
-                <div className="flex flex-col items-center text-center justify-center xl:w-1/2 2xl:w-fit xl:h-screen-50 xl:text-3xl regals
-                sm:py-10">
-                    <h1 className="sm:text-sm md:text-lg lg:text-xl">VIENEN DE</h1>
-                    <h1 className="sm:text-3xl md:text-4xl">FRANCIA</h1>
-                    <h1 className="sm:text-sm md:text-lg lg:text-xl">PARECEN DE</h1>
-                    <h1 className="sm:text-3xl md:text-4xl">ESTADOS UNIDOS</h1>
-                    <h1 className="sm:text-sm md:text-lg lg:text-xl">CON RECETA DE</h1>
-                    <h1 className="sm:text-3xl md:text-4xl">NEUQUÉN</h1>
-                    <h1 className="sm:text-sm md:text-lg lg:text-xl">HECHAS EN</h1>
-                    <h1 className="sm:text-3xl md:text-4xl">ASUNCIÓN</h1>
+            <div className="w-full h-full flex flex-col items-center z-0 pt-10">
+                <div className="h-screen-50 xl:h-screen-76 w-full px-10 lg-px-20 xl:px-36">
+                    <CarouselWithContent images={imagesMenu} />
                 </div>
-                <div className="flex flex-col lg:flex-row items-center justify-center w-full xl:h-screen-50 sm:gap-20 bg-deep-orange-200 xl:gap-44">
-                    <div className="flex-col items-center justify-center w-48 h-56 sm:py-5"><EcommerceCard></EcommerceCard></div>
-                    <div className="flex-col items-center justify-center w-48 h-56 sm:py-5"><EcommerceCard></EcommerceCard></div>
-                    <div className="flex-col items-center justify-center w-48 h-56 sm:py-5"><EcommerceCard></EcommerceCard></div>
+                <div className="flex justify-between gap-36   px-10 lg-px-20 xl:px-36 py-10 my-10">
+                    <img className="hidden lg:block w-1/4 object-cover rounded-xl shadow-xl" src={img2} alt="" />
+                    <div className="flex flex-col items-center text-center justify-center xl:text-3xl regals gap-4">
+                        <p className="text-xl">VIENEN DE</p>
+                        <p className="text-3xl">FRANCIA</p>
+                        <p className="text-xl">PARECEN DE</p>
+                        <p className="text-3xl">ESTADOS UNIDOS</p>
+                        <p className="text-xl">CON RECETA DE</p>
+                        <p className="text-3xl">NEUQUÉN</p>
+                        <p className="text-xl">HECHAS EN</p>
+                        <p className="text-3xl">ASUNCIÓN</p>
+                    </div>
+                    <img className="hidden lg:block w-1/4 object-cover rounded-xl shadow-xl" src={img3} alt="" />
                 </div>
-                <div className="flex flex-row justify-center w-full xl:h-20 sm:py-5 bg-deep-orange-200">
-                  <Link to="/menu">
-                    <Button>Nuestro menú</Button>
-                  </Link>
+                <div className="flex flex-col lg:flex-row items-center justify-center w-full xl:h-screen-50 sm:gap-20 lg:bg-deep-orange-200 xl:gap-44 ">
+                    <div className="flex-col items-center justify-center w-96 lg:w-48 lg:h-56 sm:py-5"><EcommerceCard image={img4}></EcommerceCard></div>
+                    <div className="flex-col items-center justify-center w-96 lg:w-48 lg:h-56 sm:py-5"><EcommerceCard image={img5}></EcommerceCard></div>
+                    <div className="flex-col items-center justify-center w-96 lg:w-48 lg:h-56 sm:py-5"><EcommerceCard image={img6}></EcommerceCard></div>
                 </div>
-                <div className="flex flex-col items-center justify-center xl:w-1/2 2xl:w-fit xl:h-screen-50">
-                    <h1 className="tabernaserif">¡Una forma diferente y divertida de comer waffles!</h1>
-                        <br />
-                    <h1>Encontranos en Teniente Vera esquina Denis Roa</h1>
-                        <br />  
-                    <h1>Lunes a Domingos - 12:00 a 20:00</h1>
+                <div className="flex flex-row justify-center w-full xl:h-20 py-20 xl:p-2  lg:bg-deep-orange-200">
+                    <Link to="/menu">
+                        <Button>Nuestro menú</Button>
+                    </Link>
+                </div>
+                <div className="flex flex-col items-center justify-center xl:w-1/2 2xl:w-fit xl:h-screen-50 gap-6 my-10">
+                    <p className="tabernaserif text-2xl text-center">¡Una forma diferente y divertida de comer waffles!</p>
+                    <p className="text-xl text-center ">Encontranos en Teniente Vera esquina Denis Roa</p>
+                    <p className="text-xl text-center">Lunes a Domingos - 12:00 a 20:00</p>
+                    <Link to="/menu" className="mt-5">
+                        <Button>Conoce más sobre nosotros</Button>
+                    </Link>
                 </div>
             </div>
-        </Layout>
+        </Layout >
     )
 }
